7464b0cab14e494f0dd911400b53d29fa9c69e8d
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 00:23:52 +0330

    refactor component app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-import Dashboard from './pages/Dashboard';
-import LoginPage from './pages/LoginPage';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
   return (
-    // <Router>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    // </Router>
+    <RouterProvider router={router} />
   );
 }
 
 export default App;
-
