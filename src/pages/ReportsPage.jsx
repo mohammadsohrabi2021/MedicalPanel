a9ced76f357581c50e0020c998646dc36e5754f6
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 00:26:35 +0330

    create component ReportsPage done

---
+++ src/pages/ReportsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportsPage = () => {
+  return (
+    <div>
+      <h2>گزارش‌ها</h2>
+      {/* محتوای گزارش‌ها */}
+    </div>
+  );
+};
+
+export default ReportsPage;
