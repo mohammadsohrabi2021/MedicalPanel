cd0d547b71111aae4aaccedc36a2dea90879ae28
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 00:26:48 +0330

    create component PatientsPage done

---
+++ src/pages/PatientsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PatientsPage = () => {
+  return (
+    <div>
+      <h2>لیست بیماران</h2>
+      {/* محتوای لیست بیماران */}
+    </div>
+  );
+};
+
+export default PatientsPage;
