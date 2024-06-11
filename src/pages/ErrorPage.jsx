9fe51c7dedf09858f61da29a096e6bce783170de
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 00:26:16 +0330

    create component ErrorPage done

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/ErrorPage.js
+
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>خطا</h1>
+      <p>مشکلی پیش آمده است. لطفا دوباره تلاش کنید.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
