f2c4123fd9dc3c287b08957a9bd00c410fa957bb
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 00:27:35 +0330

    create component file router done

---
+++ src/router.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import LoginPage from './pages/LoginPage';
+import PatientsPage from './pages/PatientsPage';
+import ReportsPage from './pages/ReportsPage';
+import ErrorPage from './pages/ErrorPage';
+import MainLayout from './layout/MainLayout';
+import IdentityLayout from './layout/IdentityLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IdentityLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "patients",
+        element: <PatientsPage />,
+      },
+      {
+        path: "reports",
+        element: <ReportsPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
