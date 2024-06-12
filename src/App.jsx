fc43ccf041843dd6ee8564af607060c28875111b
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-06-12 03:41:50 +0330

    refactor component App done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-
+import { ToastContainer } from 'react-toastify';
 function App() {
   return (
+    <>
     <RouterProvider router={router} />
+    <ToastContainer />
+    </>
   );
 }
 
